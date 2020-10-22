2f546c1de1d944ffaf3b6e6a1dfea39be6ca1609
Cristiano Lopes <cristiano@endlessloop.pt>
2020-10-22 15:37:11 +0100

    Set paths for dependencies

--- pteid-mw-pt/windows/set_path_autenticacao.gov.bat
+++ pteid-mw-pt/windows/set_path_autenticacao.gov.bat
@@ -1,42 +1,42 @@
 :: Compiled / external libs
 ::==============================
-@set PTEID_POPPLER_QT5=C:\git\tools\build-scripts\windows\external-libs\poppler-0.87
+@set PTEID_POPPLER_QT5=E:\64\poppler-0.87
 
 :: Compiled / external libs x86
 ::==============================
-@set PTEID_DIR_QML=C:\git\tools\build-scripts\windows\external-libs\qt5.12.8\Win32\VC9\Release\qml-app
+@set PTEID_DIR_QML=E:\32\qt5.12.8\Win32\VC9\Release\qml-app
 @set PTEID_DIR_QT_5=C:\Qt\Qt5.12.8\5.12.8\msvc2017
-@set PTEID_DIR_OPENSSL=C:\git\tools\build-scripts\windows\external-libs\openssl-1.1.1g-win32
-@set PTEID_DIR_XERCES=C:\git\tools\build-scripts\windows\external-libs\xerces-c-3.2.3-win32
-@set PTEID_XMLSECURITY=C:\git\tools\build-scripts\windows\external-libs\xml-security-c-2.0.2-win32
-@set PTEID_DIR_CURL=C:\git\tools\build-scripts\windows\external-libs\libcurl-vc-x86-release
-@set PTEID_DIR_ZLIB=C:\git\tools\build-scripts\windows\external-libs\zlib\Win32
-@set PTEID_DIR_LIBZIP=C:\git\tools\build-scripts\windows\external-libs\libzip-1.5.2\win32
-@set PTEID_DIR_LIBPNG=C:\git\tools\build-scripts\windows\external-libs\libpng-1.6.37\Win32
-@set PTEID_DIR_OPENJPEG=C:\git\tools\build-scripts\windows\external-libs\openjpeg-2.3\Win32
-@set PTEID_DIR_JPEG=C:\git\tools\build-scripts\windows\external-libs\poppler-0.87\win32
+@set PTEID_DIR_OPENSSL=E:\32\openssl
+@set PTEID_DIR_XERCES=E:\32\xerces-c
+@set PTEID_XMLSECURITY=E:\32\xmlsec
+@set PTEID_DIR_CURL=E:\32\libcurl
+@set PTEID_DIR_ZLIB=E:\32\zlib
+@set PTEID_DIR_LIBZIP=E:\32\libzip
+@set PTEID_DIR_LIBPNG=E:\32\libpng
+@set PTEID_DIR_OPENJPEG=E:\32\openjpeg
+@set PTEID_DIR_JPEG=E:\32\poppler-0.87
 
 :: Compiled / external libs x64
 ::==============================
-@set PTEID_DIR_QML_X64=C:\git\tools\build-scripts\windows\external-libs\qt5.12.8\x64\VC9\Release\qml-app
+@set PTEID_DIR_QML_X64=E:\64\qt5.12.8\x64\VC9\Release\qml-app
 @set PTEID_DIR_QT_5_X64=C:\Qt\Qt5.12.8\5.12.8\msvc2017_64
-@set PTEID_DIR_OPENSSL_X64=C:\git\tools\build-scripts\windows\external-libs\openssl-1.1.1g-x64
-@set PTEID_DIR_XERCES_X64=C:\git\tools\build-scripts\windows\external-libs\xerces-c-3.2.3-win64
-@set PTEID_XMLSECURITY_X64=C:\git\tools\build-scripts\windows\external-libs\xml-security-c-2.0.2-win64
-@set PTEID_DIR_CURL_X64=C:\git\tools\build-scripts\windows\external-libs\libcurl-vc-x64-release
-@set PTEID_DIR_ZLIB_X64=C:\git\tools\build-scripts\windows\external-libs\zlib\x64
-@set PTEID_DIR_LIBZIP_X64=C:\git\tools\build-scripts\windows\external-libs\libzip-1.5.2\x64
-@set PTEID_DIR_LIBPNG_X64=C:\git\tools\build-scripts\windows\external-libs\libpng-1.6.37\x64
-@set PTEID_DIR_OPENJPEG_X64=C:\git\tools\build-scripts\windows\external-libs\openjpeg-2.3\x64
-@set PTEID_DIR_JPEG_X64=C:\git\tools\build-scripts\windows\external-libs\poppler-0.87\x64
+@set PTEID_DIR_OPENSSL_X64=E:\64\openssl
+@set PTEID_DIR_XERCES_X64=E:\64\xerces-c
+@set PTEID_XMLSECURITY_X64=E:\64\xmlsec
+@set PTEID_DIR_CURL_X64=E:\64\libcurl
+@set PTEID_DIR_ZLIB_X64=E:\64\zlib
+@set PTEID_DIR_LIBZIP_X64=E:\64\libzip
+@set PTEID_DIR_LIBPNG_X64=E:\64\libpng
+@set PTEID_DIR_OPENJPEG_X64=E:\64\openjpeg
+@set PTEID_DIR_JPEG_X64=E:\64\poppler-0.87
 
 :: Tools
 ::======
 @set PTEID_DIR_7ZIP=C:\Program Files\7-Zip
 @set PTEID_DIR_CRYPTPDK=C:\Program Files (x86)\Windows Kits\8.0\Cryptographic Provider Development Kit
 @set PTEID_DIR_VS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
-@set PTEID_DIR_DOXYGEN=C:\Program Files\doxygen-1.8.15.windows.x64.bin
-@set PTEID_DIR_SWIG=C:\swigwin-4.0.1
+@set PTEID_DIR_DOXYGEN=C:\Program Files\doxygen\bin
+@set PTEID_DIR_SWIG=E:\swigwin
 @set PTEID_DIR_JDK=C:\Program Files\Java\jdk1.8.0_231
 @set PTEID_DIR_WIX=C:\Program Files (x86)\WiX Toolset v3.11
 @set PTEID_DIR_MSM=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.16.27012\MergeModules
